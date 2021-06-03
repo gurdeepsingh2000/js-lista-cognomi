3ce1d13bfb2d93ab5feef7db9dc55751780dc39d
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-03 16:15:43 +0200

    esercizio finito

--- js/my-script.js
+++ js/my-script.js
@@ -1,17 +1,20 @@
 var all_surnames = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli']
 var output = document.getElementById('surnames');
 
+var surname_request = prompt('Inserisci il tuo cognome');
+all_surnames.push(surname_request);
+var rank = 0;
 
-var n = all_surnames.push(prompt('Inserisci il tuo cognome'));
-all_surnames.sort()
+all_surnames.sort();
 
 for(var x = 0; x < all_surnames.length ; x++) { 
     output.innerHTML += all_surnames[x] + '<br>' ;
+    if(all_surnames[x] === surname_request){
+        rank = x+1;
+    }
+
 }
 
-output.innerHTML += all_surnames.indexOf(n) +'<br>' ;
 
 
-
-
-
+output.innerHTML += 'La tua posizione nella lista è ' + rank +'<br>' ;
