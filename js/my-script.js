aba9acf4a167f4e8276fe77d13d589c763a15dd5
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-03 15:26:23 +0200

    update

--- js/my-script.js
+++ js/my-script.js
@@ -1,12 +1,16 @@
 var all_surnames = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli']
 var output = document.getElementById('surnames');
 
-all_surnames.push(prompt('Inserisci il tuo cognome'));
+
+var n = all_surnames.push(prompt('Inserisci il tuo cognome'));
 all_surnames.sort()
 
 for(var x = 0; x < all_surnames.length ; x++) { 
+    output.innerHTML += all_surnames[x] + '<br>' ;
 }
-output.innerHTML = all_usernames[x];
+
+output.innerHTML += all_surnames.indexOf(n) +'<br>' ;
+
 
 
 
